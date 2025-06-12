5eed1a62a327ff35423b5e824f02f2557628255b
Wei-Cheng Tseng <weichengt@nvidia.com>
2025-06-12 11:28:50 -0700

    update model and network

---
+++ cosmos_predict2/models/video2world_model_action.py
@@ -0,0 +1,162 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math
+
+import attrs
+import torch
+from megatron.core import parallel_state
+from torch.distributed.device_mesh import init_device_mesh
+
+from cosmos_predict2.configs.action_conditional.config_action_conditional import (
+    ACTION_CONDITIONAL_PREDICT2_VIDEO2WORLD_PIPELINE_2B,
+    ActionConditionalVideo2WorldPipelineConfig,
+)
+from cosmos_predict2.pipelines.video2world_action import ActionConditionalVideo2WorldPipeline
+from cosmos_predict2.utils.checkpointer import non_strict_load_model
+from cosmos_predict2.utils.optim_instantiate import get_base_scheduler
+from cosmos_predict2.models.video2world_model import Predict2Video2WorldModel
+from imaginaire.lazy_config import LazyDict, instantiate
+from imaginaire.utils import log
+
+
+@attrs.define(slots=False)
+class Predict2ModelManagerConfig:
+    # Local path, use it in fast debug run
+    dit_path: str = "checkpoints/nvidia/Cosmos-Predict2-2B-Video2World/model-720p-16fps.pt"
+    dit_ema_path: str = "checkpoints/nvidia/Cosmos-Predict2-2B-Video2World/model-720p-16fps.pt"
+    # For inference
+    text_encoder_path: str = ""  # not used in training.
+
+
+@attrs.define(slots=False)
+class Predict2Video2WorldModelConfig:
+    learning_rate: float = 2 ** (-14.5)
+    train_architecture: str = "base"
+    use_gradient_checkpointing: bool = True
+    use_gradient_checkpointing_offload: bool = False
+    use_selective_activation_checkpointing: bool = False
+    compute_latents_online: bool = False
+    num_video_frames: int = 81
+    resolution: str = "720"
+
+    precision: str = "bfloat16"
+    input_data_key: str = "video"
+    input_image_key: str = "images"
+    loss_reduce: str = "mean"
+    loss_scale: float = 10.0
+
+    adjust_video_noise: bool = True
+
+    # This is used for the original way to load models
+    model_manager_config: Predict2ModelManagerConfig = Predict2ModelManagerConfig()
+    # This is a new way to load models
+    pipe_config: ActionConditionalVideo2WorldPipelineConfig = ACTION_CONDITIONAL_PREDICT2_VIDEO2WORLD_PIPELINE_2B
+    # debug flag
+    debug_without_randomness: bool = False
+    fsdp_shard_size: int = 0  # 0 means not using fsdp, -1 means set to world size
+    # High sigma strategy
+    high_sigma_ratio: float = 0.0
+
+
+class ActionConditionalPredict2Video2WorldModel(Predict2Video2WorldModel):
+    def __init__(self, config: Predict2Video2WorldModelConfig):
+        # super().__init__()
+        # New code, added for i4 adaption
+        learning_rate = config.learning_rate
+        use_gradient_checkpointing = config.use_gradient_checkpointing
+        use_gradient_checkpointing_offload = config.use_gradient_checkpointing_offload
+
+        self.config = config
+
+        self.precision = {
+            "float32": torch.float32,
+            "float16": torch.float16,
+            "bfloat16": torch.bfloat16,
+        }[config.precision]
+        self.tensor_kwargs = {"device": "cuda", "dtype": self.precision}
+        self.device = torch.device("cuda")
+
+        # 1. set data keys and data information
+        self.setup_data_key()
+
+        # 4. Set up loss options, including loss masking, loss reduce and loss scaling
+        self.loss_reduce = getattr(config, "loss_reduce", "mean")
+        assert self.loss_reduce in ["mean", "sum"]
+        self.loss_scale = getattr(config, "loss_scale", 1.0)
+        log.critical(f"Using {self.loss_reduce} loss reduce with loss scale {self.loss_scale}")
+        if self.config.adjust_video_noise:
+            self.video_noise_multiplier = math.sqrt(self.config.pipe_config.state_t)
+        else:
+            self.video_noise_multiplier = 1.0
+
+        # 7. training states
+        if parallel_state.is_initialized():
+            self.data_parallel_size = parallel_state.get_data_parallel_world_size()
+        else:
+            self.data_parallel_size = 1
+
+        # replace the pipeline with action-conditioning
+        self.pipe = ActionConditionalVideo2WorldPipeline.from_config(
+            config.pipe_config,
+            dit_path=config.model_manager_config.dit_path,
+        )
+
+        self.freeze_parameters()
+        if config.train_architecture == "lora":
+            self.add_lora_to_model(
+                self.pipe.dit,
+                lora_rank=config.lora_rank,
+                lora_alpha=config.lora_alpha,
+                lora_target_modules=config.lora_target_modules,
+                init_lora_weights=config.init_lora_weights,
+            )
+            if self.pipe.dit_ema:
+                self.add_lora_to_model(
+                    self.pipe.dit_ema,
+                    lora_rank=config.lora_rank,
+                    lora_alpha=config.lora_alpha,
+                    lora_target_modules=config.lora_target_modules,
+                    init_lora_weights=config.init_lora_weights,
+                )
+        else:
+            self.pipe.denoising_model().requires_grad_(True)
+        total_params = sum(p.numel() for p in self.parameters())
+        frozen_params = sum(p.numel() for p in self.parameters() if not p.requires_grad)
+        trainable_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        # Print the number in billions, or in the format of 1,000,000,000
+        log.info(
+            f"Total parameters: {total_params / 1e9:.2f}B, Frozen parameters: {frozen_params:,}, Trainable parameters: {trainable_params:,}"
+        )
+
+        if config.fsdp_shard_size != 0 and torch.distributed.is_initialized():
+            if config.fsdp_shard_size == -1:
+                fsdp_shard_size = torch.distributed.get_world_size()
+                replica_group_size = 1
+            else:
+                fsdp_shard_size = min(config.fsdp_shard_size, torch.distributed.get_world_size())
+                replica_group_size = torch.distributed.get_world_size() // fsdp_shard_size
+            dp_mesh = init_device_mesh(
+                "cuda", (replica_group_size, fsdp_shard_size), mesh_dim_names=("replicate", "shard")
+            )
+            log.info(f"Using FSDP with shard size {fsdp_shard_size} | device mesh: {dp_mesh}")
+            self.pipe.apply_fsdp(dp_mesh)
+        else:
+            log.info("FSDP (Fully Sharded Data Parallel) is disabled.")
+
+        self.learning_rate = learning_rate
+        self.use_gradient_checkpointing = use_gradient_checkpointing
+        self.use_gradient_checkpointing_offload = use_gradient_checkpointing_offload
+
