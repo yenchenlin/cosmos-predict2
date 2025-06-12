3c4f039a7ff7723c0e1e10a5cc98f00ff6652bdf
Wei-Cheng Tseng <weichengt@nvidia.com>
2025-06-12 10:41:48 -0700

    update action-conditioning

---
+++ cosmos_predict2/configs/action_conditional/defaults/conditioner.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass
+from typing import Dict, Optional
+
+import torch
+from hydra.core.config_store import ConfigStore
+
+from imaginaire.lazy_config import LazyCall as L
+from imaginaire.lazy_config import LazyDict
+from cosmos_predict2.configs.vid2vid.defaults.conditioner import Vid2VidCondition, Vid2VidConditioner
+
+
+@dataclass(frozen=True)
+class ActionConditionalCondition(Vid2VidCondition):
+    action: Optional[torch.Tensor] = None
+
+
+class ActionConditionalConditioner(Vid2VidConditioner):
+    def forward(
+        self,
+        batch: Dict,
+        override_dropout_rate: Optional[Dict[str, float]] = None,
+    ) -> ActionConditionalCondition:
+        output = super()._forward(batch, override_dropout_rate)
+        assert "action" in batch, "ActionConditionalConditioner requires 'action' in batch"
+        output["action"] = batch["action"]
+        return ActionConditionalCondition(**output)
---
+++ cosmos_predict2/configs/action_conditional/defaults/data.py
@@ -0,0 +1,100 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from hydra.core.config_store import ConfigStore
+from megatron.core import parallel_state
+from torch.utils.data import DataLoader, DistributedSampler
+
+from cosmos_predict2.data.action_conditional.action_conditional_dataset import ActionConditionalDataset
+from imaginaire.lazy_config import LazyCall as L
+
+base_path = "./datasets/bridge/"
+train_annotation_path = os.path.join(base_path, "annotation/train")
+val_annotation_path = os.path.join(base_path, "annotation/val")
+test_annotation_path = os.path.join(base_path, "annotation/test")
+
+
+bridge_train_dataset = L(ActionConditionalDataset)(
+    train_annotation_path=train_annotation_path,
+    val_annotation_path=val_annotation_path,
+    test_annotation_path=test_annotation_path,
+    video_path=base_path,
+    sequence_interval=1,
+    num_frames=13,
+    cam_ids=[0],
+    accumulate_action=False,
+    video_size=[256, 320],
+    val_start_frame_interval=1,
+    mode="train",
+)
+
+bridge_val_dataset = L(ActionConditionalDataset)(
+    train_annotation_path=train_annotation_path,
+    val_annotation_path=val_annotation_path,
+    test_annotation_path=test_annotation_path,
+    video_path=base_path,
+    sequence_interval=1,
+    num_frames=13,
+    cam_ids=[0],
+    accumulate_action=False,
+    video_size=[256, 320],
+    val_start_frame_interval=1,
+    mode="val",
+)
+
+
+def get_sampler(dataset):
+    return DistributedSampler(
+        dataset,
+        num_replicas=parallel_state.get_data_parallel_world_size(),
+        rank=parallel_state.get_data_parallel_rank(),
+        shuffle=True,
+        seed=0,
+    )
+
+
+bridge_train_dataloader = L(DataLoader)(
+    dataset=bridge_train_dataset,
+    sampler=L(get_sampler)(dataset=bridge_train_dataset),
+    batch_size=1,
+    drop_last=True,
+)
+
+bridge_val_dataloader = L(DataLoader)(
+    dataset=bridge_val_dataset,
+    sampler=L(get_sampler)(dataset=bridge_val_dataset),
+    batch_size=1,
+    drop_last=True,
+)
+
+
+def register_training_and_val_data():
+    cs = ConfigStore.instance()
+
+    # for local dataset
+    cs.store(
+        group="data_train",
+        package="dataloader_train",
+        name="bridge_train",
+        node=bridge_train_dataloader,
+    )
+    cs.store(
+        group="data_val",
+        package="dataloader_val",
+        name="bridge_val",
+        node=bridge_val_dataloader,
+    )
