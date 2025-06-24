b9a36f282e031f1415cf2a9cbd4dd52ead11ac28
Yen-Chen Lin <yenchenl@nvidia.com>
2025-06-23 18:21:04 -0700

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+clear
+EXP=predict2_video2world_training_2b_groot_gr1_480_mock_data
+torchrun --nproc_per_node=8 --master_port=12341 -m scripts.train --config=cosmos_predict2/configs/base/config.py -- experiment=${EXP}
+rm -rf /project/cosmos/yenchenl/imaginaire4-output/posttraining/video2world/2b_groot_gr1_480/
+torchrun --nproc_per_node=8 --master_port=12341 -m scripts.train --config=cosmos_predict2/configs/base/config.py -- experiment=${EXP}
+rm -rf /project/cosmos/yenchenl/imaginaire4-output/posttraining/video2world/2b_groot_gr1_480/
